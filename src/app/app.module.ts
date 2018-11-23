542bda6c18841ff78adc30ce69bd114b0e043535
leonardo.aires <leonardo.aires@centralit.com.br>
2018-11-23 17:45:09 -0200

    Alterações do formulario Lancamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccordionModule } from 'primeng/accordion';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DataTableModule } from 'primeng/components/datatable/datatable';
 
 @NgModule({
   declarations: [
@@ -10,9 +17,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
